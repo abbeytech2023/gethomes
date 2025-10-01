5ef2e2d5bec5ef29f1a1094f57415b598927a01f
olanipekun abiodun <olanipekunabbey@gmail.com>
2025-10-01 02:54:18 -0700

    first commit

--- src/services/supabaseClients.js
+++ src/services/supabaseClients.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
-// export const supabaseUrl = "https://eluekkplzzlsspmxmoky.supabase.co";
+export const supabaseUrl = "https://eluekkplzzlsspmxmoky.supabase.co";
 
 // const supabaseKey =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsdWVra3Bsenpsc3NwbXhtb2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5NTAzNjcsImV4cCI6MjA2MzUyNjM2N30.MxYwxrYUwA4HVP3CRGvRl8hfia6qZWmQHC-KhQgrrZs";
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
